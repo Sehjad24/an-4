23c3a23cb2d5d01e98d60ca115a1a20e0eb6cea5
Sehjad24 <sehjad24@proton.me>
2023-03-09 22:15:41 +0600

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MongoURI: "mongodb+srv://usermemaximumsomo:usermemaxf4454imumsomo@cluster0.3igft7l.mongodb.net/test?ssl=true&replicaSet=atlas-qkusun-shard-0&authSource=admin&retryWrites=true&w=majority"
+};

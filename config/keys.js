dddcf68da802049e5370d7254a722a9c1a431fa9
Sehjad24 <sehjad24@proton.me>
2023-03-09 22:19:05 +0600

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  MongoURI: "mongodb+srv://usermemaximumsomo:usermemaxf4454imumsomo@cluster0.3igft7l.mongodb.net/test?ssl=true&replicaSet=atlas-qkusun-shard-0&authSource=admin&retryWrites=true&w=majority"
+  MongoURI: "mongodb+srv://selim24:ojQk3VQuf8iwTysg@cluster4.yobsy44.mongodb.net/test?ssl=true&replicaSet=atlas-qkusun-shard-0&authSource=admin&retryWrites=true&w=majority"
 };
